766359ed9ffb1ab263f354dffb32e2a38ee49d8b
ValleysEnduro <info@western-bike-events.wales>
2024-09-01 21:50:12 +0100

    scaffolding

---
+++ source/conf.py
@@ -0,0 +1,29 @@
+#
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Templot5 Companion'
+copyright = '2024, Richard Bunting, working on behalf of Martin Wynne'
+author = 'Richard Bunting, working on behalf of Martin Wynne'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
